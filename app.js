cd0b483988eff3480ea3468f6c0c537e0718d595
Alex R <areniak147@hotmail.com>
2019-01-24 09:43:50 -0500

    basic HW done

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-//create default search values for spotify and movie APIS
-// take in data from random.txt and run spotify search with input
+// create default search values for spotify and movie APIS
 
 // Environment variables for spotify API
 require("dotenv").config();
@@ -16,14 +15,15 @@ const spotify = new Spotify({
 
 const fs = require("fs");
 const command = process.argv[2];
-const userInput = process.argv.splice(3,process.argv.length).join("+");
+let userInput = process.argv.splice(3,process.argv.length).join("+");
 
-
-// Commands from node:
+console.log(userInput)
+// Commands from terminal:
 
 // concert-this function
 // run user input in bandsintown api and return results about event
 function concertThis() {
+  console.log("-------------------------");
   axios
     .get(`https://rest.bandsintown.com/artists/${userInput}/events?app_id=codingbootcamp`)
     .then(response => {
@@ -31,7 +31,7 @@ function concertThis() {
       let dateConvert = JSON.stringify(response.data[0].datetime);
       datePerform = moment(dateConvert, "YYYY-MM-DDTHH:mm:ss").format("MM/DD/YYYY")
       console.log(datePerform);
-      console.log("-------------------------")
+      console.log("-------------------------");
     })
     .catch(error => console.log(error));
 };
@@ -39,25 +39,26 @@ function concertThis() {
 // spotify-this-song function
 // take in user input and return data about the song searched
 function spotifyThisSong(input) {
+  console.log("-------------------------");
   spotify
     .request(`https://api.spotify.com/v1/search?q=${input}&type=track&limit=1`)
     .then(data => {
       console.log(
-        `Song: ${data.tracks.items[0].name} \nAlbum: ${data.tracks.items[0].album.name} \nBand: ${data.tracks.items[0].artists[0].name}  \nPreview the song here: ${data.tracks.items[0].preview_url}`)
-        console.log("-------------------------")
+        `Song: ${data.tracks.items[0].name} \nAlbum: ${data.tracks.items[0].album.name} \nBand: ${data.tracks.items[0].artists[0].name}  \nPreview the song here: ${data.tracks.items[0].preview_url}`);
+        console.log("-------------------------");
     })    
     .catch(error => console.log(error));
-
 }
 
 // movie-this function
 // take in user input and return data about the movie that was searched
 function movieThis(input) {
+  console.log("-------------------------");
   axios
     .get(`http://www.omdbapi.com/?apikey=trilogy&t=${input}`)
     .then(response => {
       console.log(`Title: ${response.data.Title} \nYear released: ${response.data.Year} \nIMDB rating: ${response.data.imdbRating} \nRotton Tomatoes rating: ${response.data.Ratings[1].Value} \nCountry movie was produced: ${response.data.Country} \nLanguage: ${response.data.Language} \nPlot: ${response.data.Plot} \nActors: ${response.data.Actors}`);
-      console.log("-------------------------")
+      console.log("-------------------------");
     })
     .catch(error => console.log(error));
 };
@@ -65,14 +66,16 @@ function movieThis(input) {
 // do-what-it-says function
 // take song from random.txt file and use it as search paramenter for spotify API
 function doWhatItSays() {
+  console.log("-------------------------");
   fs.readFile("random.txt", "utf8", (error, data) => {
     if (error) {
-      return console.log(error)
+      return console.log(error);
     };
     const dataArr = data.split(",");
     const spotifyInput = dataArr[1].replace(/ /g, "+");
 
     spotifyThisSong(spotifyInput);
+    console.log("-------------------------")
   });
 };
 
@@ -84,18 +87,13 @@ switch (command) {
   break;
 
   case "spotify-this-song": {
-    console.log(userInput)
-    let defaultInput = "The Sign"
-    let defaultSearch = defaultInput.replace(/ /g, "+")
-    (userInput != undefined) ? spotifyThisSong(userInput) : spotifyThisSong(defaultSearch)
-    // spotifyThisSong(userInput);
+    userInput = userInput || "The Sign";
+    spotifyThisSong(userInput);
     }
   break;
 
   case "movie-this":
-    // let defaultInput = "Mr. Nodoby";
-    // let defaultSearch = defaultInput.split(" ").join("+");
-    // (userInput != "") ? movieThis(userInput) : movieThis(defaultSearch)
+    userInput = userInput || "Mr. Nobody"
     movieThis(userInput);
   break;
 
